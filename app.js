7d97405e30cf6ec12b07084290d4c91b4a762cea
MARC LOOS <Marc.Loos@nl.ibm.com>
2019-06-05 12:31:51 +0000

    test logDNA

--- app.js
+++ app.js
@@ -5,6 +5,30 @@ var bodyParser = require('body-parser');
 var apiv1 = require('./routes/apiv1.js');
 var EJS = require('ejs');
 
+var Logger = require('logdna');
+var options = {
+    hostname: myHostname,
+    ip: ipAddress,
+    mac: macAddress,
+    app: appName,
+    env: envName
+};
+var apikey=9b631129805c57c9b36a14471dbd35f2
+
+// Defaults to false, when true ensures meta object will be searchable
+options.index_meta = true;
+
+// Add tags in array or comma-separated string format:
+options.tags = ['logging', 'nodejs', 'logdna'];
+// or:
+options.tags = 'logging,nodejs,logdna';
+
+// Define a singleton instance
+var logger = Logger.setupDefaultLogger(apikey, options);
+
+// Create multiple loggers with different options
+var logger = Logger.createLogger(apikey, options);
+
 EJS.open = "<ejs>";
 EJS.close = "</ejs>";
 
